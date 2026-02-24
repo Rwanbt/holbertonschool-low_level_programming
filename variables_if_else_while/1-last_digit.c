d4a0b5aeba71296e9182831c6fc3339b36f6417f
Rwanbt <barat.erwan@gmail.com>
2026-02-24 10:19:29 +0100

    Fix logic and formatting for task 1 and update previous tasks

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,36 +1,42 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last_digit;
-	/* ... génération de n ... */
-
+	/* 1. Calculer le dernier chiffre ici */
 	last_digit = n % 10;
+
+	/* 2. Afficher le début commun de la phrase */
+	printf("Last digit of %d is %d ", n, last_digit);
 	
-	if (last_digit > 0)
+	/* 3. Ajouter la fin de la phrase selon la condition */
+	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than\n", n, last_digit);
+		printf("and is greater than 5\n");
 	}
 
-	else if (last_digit == 5)
+	else if (last_digit == 0)
 	{
-		printf("Lasr digit of %d is %d and is less than\n", n, last_digit);
-	}
-	else (last_digit < 6, last_digit != 0)
-	{
-		printf("Last digit of %d is %d and is %d\n", n, last_digit);
+		printf("and is 0\n");
 	}
 	
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
 	return (0);
-
-	int last_digit;
 }
 
