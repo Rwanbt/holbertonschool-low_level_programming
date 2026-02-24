f36efedc7fa9dfa2779c4c9b47632873b335cee6
Rwanbt <barat.erwan@gmail.com>
2026-02-24 14:31:53 +0100

    feat numbers base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c< = '0')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
