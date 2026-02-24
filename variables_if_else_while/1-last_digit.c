2d1059f85da8d1a379bfcd220eaab2f0577bfa4c
Rwanbt <barat.erwan@gmail.com>
2026-02-24 10:27:09 +0100

    Fix2 betty last digit number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,28 +14,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	/* 1. Calculer le dernier chiffre ici */
 	last_digit = n % 10;
 
-	/* 2. Afficher le début commun de la phrase */
 	printf("Last digit of %d is %d ", n, last_digit);
-	
-	/* 3. Ajouter la fin de la phrase selon la condition */
 	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-
 	else if (last_digit == 0)
 	{
 		printf("and is 0\n");
 	}
-	
 	else
 	{
 		printf("and is less than 6 and not 0\n");
 	}
-
 	return (0);
 }
