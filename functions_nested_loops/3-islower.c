429554b60dd1c9b5db4f675d2751222522a26350
Rwanbt <barat.erwan@gmail.com>
2026-02-25 14:44:23 +0100

    fix betty islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _islower - Checks for lowercase character
+ * @c: The character to check
  *
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
