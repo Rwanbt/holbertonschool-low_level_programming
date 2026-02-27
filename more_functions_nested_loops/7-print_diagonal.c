f6ee0eff1e1bc4ac9e6678ec0a4741b80316776b
Rwanbt <barat.erwan@gmail.com>
2026-02-27 15:05:42 +0100

    feat print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints line
+ * @n: The number of print
+ *
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space < n; space++)
+			{
+			_putchar(' ')
+			}
+			_putchar('\\')
+			_putchar('\n')
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
