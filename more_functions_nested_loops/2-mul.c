6b403d71ff13a7c85778d2802a7aaacb29d63c00
Rwanbt <barat.erwan@gmail.com>
2026-02-27 10:51:21 +0100

    feat multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: The number to be multiplies
+ * @b: The number to be multiplies
+ *
+ * Return: The result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
