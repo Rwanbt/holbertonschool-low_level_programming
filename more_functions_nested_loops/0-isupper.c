443cb61e198600a0f4cb582452c7fdb16c15940b
Rwanbt <barat.erwan@gmail.com>
2026-02-27 09:43:21 +0100

    feat uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Checks for uppercase character
+ * 
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	return (0);
+}
