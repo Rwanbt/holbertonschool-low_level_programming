1be66d1645d892b6d0befc25f124b252634c9a92
Rwanbt <barat.erwan@gmail.com>
2026-03-03 16:22:33 +0100

    feat all operations

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -20,17 +20,39 @@ int main(void)
         }
         else if (enter > 0 && enter < 5)
         {
-            printf("Opérations: %d\n", enter);
             if (enter == 1)
             {
                 scanf("%d", &a);
                 scanf("%d", &b);
                 printf("result: %d\n", a + b);
             }
+            if (enter == 2)
+            {
+                scanf("%d", &a);
+                scanf("%d", &b);
+                printf("result: %d\n", a - b);
+            }
+            if (enter == 3)
+            {
+                scanf("%d", &a);
+                scanf("%d", &b);
+                printf("result: %d\n", a * b);
+            }
+            if (enter == 4)
+            {
+                scanf("%d", &a);
+                scanf("%d", &b);
+                if (b == 0)
+                {
+                    printf("Invalid division, try again.\n");
+                }
+                else
+                printf("result: %d\n", a / b);
+            }
         }
         else
         {
-            printf("Invalid choice, try again. \n");
+            printf("Invalid choice, try again.\n");
         }
     }
     while (enter != 0);
