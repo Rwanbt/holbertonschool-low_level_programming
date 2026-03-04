2192e0393ad980b5414686810dd40100999c1e09
Rwanbt <barat.erwan@gmail.com>
2026-03-04 16:03:21 +0100

    feat puts characters jump 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string,
+ * starting with the first character, followed by a new line
+ * @str: the string to bes treated
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putcharstr[i]);
+	}
+	_putchar('\n');
+}
