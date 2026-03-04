f19893044e28f08577d3c2a6a59e3e01537b1db9
Rwanbt <barat.erwan@gmail.com>
2026-03-04 17:00:18 +0100

    feat print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer the first element of the array
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf(", ");
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
