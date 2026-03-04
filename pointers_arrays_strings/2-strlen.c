5c1d778c4e5f6eebdb9cfa31cec205c779579c5b
Rwanbt <barat.erwan@gmail.com>
2026-03-04 14:04:15 +0100

    feat strings length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	temp = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	return (count);
+}
+
