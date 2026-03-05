8c91f863cb8c956c9d1d869156d4dfd139ef18b2
Rwanbt <barat.erwan@gmail.com>
2026-03-05 21:06:46 +0100

    fix: betty string compare

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  * _strcmp - compares two strings
  * @s1: first string to compare
- * s2: second string to compare
+ * @s2: second string to compare
  *
  * Return: the difference between the first differing characters
  */
