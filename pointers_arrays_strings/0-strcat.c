d1632262aeeb97c7ae3d3bb2c14550196df3ffbe
Rwanbt <barat.erwan@gmail.com>
2026-03-05 15:00:13 +0100

    feat:  concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i = 0;
+    int j = 0
+
+    while (dest [i] != '\0')
+    {
+        i++;
+    }
+
+    while (scr[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return (dest);
+}
