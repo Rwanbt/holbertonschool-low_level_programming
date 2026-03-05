7fc5cd06d3b1925ab81099fca5f63f622dd681a3
Rwanbt <barat.erwan@gmail.com>
2026-03-05 15:18:50 +0100

    fix: 0-strcat tab betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0
+	int i = 0;
+	int j = 0;
 
-    while (dest [i] != '\0')
-    {
-        i++;
-    }
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    while (scr[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (scr[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return (dest);
+	return (dest);
 }
