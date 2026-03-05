891af06e00d5ef2f66fa8c9d8060f492f3d9ed0b
Rwanbt <barat.erwan@gmail.com>
2026-03-05 22:06:00 +0100

    feat: change lowercase to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper -  changes all lowercase letters of a string to uppercase
+ * @str: the string to be modified
+ *
+ * Return: 0
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
