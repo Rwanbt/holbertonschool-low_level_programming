df470b1f662429046910560a8c35019b24f3b52d
Rwanbt <barat.erwan@gmail.com>
2026-03-06 11:56:00 +0100

    feat: order description and ;

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @n: number of bytes of memory area
  * @s: pointer of memory
  * @b: constant byte
+ * @n: number of bytes of memory area
  *
  * Return: s
  */
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0
+	i = 0;
 
 	for (i = 0; i < n; i++)
 	{
