389915d66fdaccf6c6d2c2d523a97bf8e4d062ac
Rwanbt <barat.erwan@gmail.com>
2026-03-09 11:16:40 +0100

    fix: betty and error text

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,9 +19,9 @@ char *create_array(unsigned int size, char c)
 	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
-		reutrn (NULL);
+		return (NULL);
 
-	for (i = 0; i < size; i++);
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c
 	}
