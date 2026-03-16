7d8f4f614fb88c8f53c7adbd7dc685a90f7dcff8
Rwanbt <barat.erwan@gmail.com>
2026-03-16 18:28:49 +0100

    fix: print dog betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -8,11 +7,11 @@
  *
  * Return: Always 0
  */
-void print_dog(struct dog *d);
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\\n", d->name ? d->name : "(nil)");
-	printf("Age: %f\\n", d->age);
-	printf("Owner: %s\\n", d ->owner ? d->owner : "(nil)");
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
