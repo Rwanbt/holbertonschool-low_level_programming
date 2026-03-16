b24790cfd83da18336159b07a5f2bdac1a869d6a
Rwanbt <barat.erwan@gmail.com>
2026-03-16 12:39:05 +0100

    Fix: str_concat  betty initalisation and logic

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,23 +6,24 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return:
+ * Return: pointer to the newly allocated space in memory, or NULL if it fails
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned len1, len2, i;
+	unsigned int len1 = 0, len2 = 0, i, j;
 	char *concat;
 
 	if (s1 == NULL)
-	s1 = "";
+		s1 = "";
 
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
 	concat = malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (concat == NULL)
@@ -32,11 +33,12 @@ char *str_concat(char *s1, char *s2)
 	{
 		concat[i] = s1[i];
 	}
-	for (i = 0; i < len2; i++)
+	for (j = 0; j < len2; j++)
 	{
-		concat[len1 + i] = s2[i];
+		concat[i] = s2[j];
+		i++;
 	}
-	concat[len1 + len2] = '\0';
+	concat[i] = '\0';
 
 	return (concat);
 }
