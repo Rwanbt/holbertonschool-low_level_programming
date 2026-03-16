f551cc66bc1ac7a78206ed3e7426e05cb6933c65
Rwanbt <barat.erwan@gmail.com>
2026-03-16 17:20:50 +0100

    fix: dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,11 +6,19 @@
  * @name: the dog's name
  * @age: the dog's age
  * @owner: the dog's owner
+ *
+ * Description: this structure store the data identity of do's
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-#endif /* DOG_H */
+};
+typedef dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
