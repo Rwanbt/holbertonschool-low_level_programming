2736d002432dec083be2371d7613262c4894cf4e
Rwanbt <barat.erwan@gmail.com>
2026-03-16 18:54:37 +0100

    fix: free dog lib and betty

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,11 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * free_dog - frees dogs
  * @d: pointer of struct dog
  *
- * Return: Always 0
+ * Return: Nothing.
  */
 void free_dog(dog_t *d)
 {
