764aeadaf8b733458b35f3b79b29898a41337b12
Rwanbt <barat.erwan@gmail.com>
2026-03-16 18:16:13 +0100

    fix: init dog maj return and lib

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stddef.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -8,7 +7,7 @@
  * @age: age of dog's
  * @owner: owner of dog's
  *
- * return: Nothing.
+ * Return: Nothing.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
