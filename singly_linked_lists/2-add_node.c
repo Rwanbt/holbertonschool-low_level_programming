1e1589bb2a895a7ccbf773ff85c52b757cd7c063
Rwanbt <barat.erwan@gmail.com>
2026-03-26 22:08:05 +0100

    Fix: Betty add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list _t list
+ * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *
