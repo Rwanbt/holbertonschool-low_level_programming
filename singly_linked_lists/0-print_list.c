8e75c86712961c960ef17d7376af6ebe218c171b
Rwanbt <barat.erwan@gmail.com>
2026-03-26 16:51:11 +0100

    Feat: print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio>
+#include "lists.h"
+
+/**
+ * print_list - Display all elements of list_t.
+ * @h: Pointer to head of list.
+ *
+ * Return: The number of node.
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	wihle (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
