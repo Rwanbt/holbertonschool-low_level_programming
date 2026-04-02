e0a19062ebf5324fe3a442d8357b97d1bccc5c7e
Rwanbt <barat.erwan@gmail.com>
2026-04-02 15:43:17 +0200

    Fix: betty typo description key_index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,10 +3,10 @@
 
 /**
  * key_index - Give the index of a key
- * @hey: The key of hash table
+ * @key: The key of hash table
  * @size: The size of table
  *
- * Return: index at which the key/value pair should be stored in the array of the hash table
+ * Return: index at which the key pair be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
