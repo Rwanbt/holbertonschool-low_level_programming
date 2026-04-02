bb2192b3ea244c874ae7ced501a4b3f701705d5f
Rwanbt <barat.erwan@gmail.com>
2026-04-02 21:46:22 +0200

    Feat: delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to the hash table.
+ *
+ * Return: void.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *next_node;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			next_node = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = next_node;
+		}
+	}
+
+	free(ht->array);
+
+	free(ht);
+}
