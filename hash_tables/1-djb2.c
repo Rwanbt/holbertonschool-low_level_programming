f1b69c1f03636907ddb4d7747a6f2d1a850a30bd
Rwanbt <barat.erwan@gmail.com>
2026-04-02 14:32:08 +0200

    Fix: Betty description djb2

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 /**
- * hash_djb2 -
+ * hash_djb2 - Algorythm implementation
  * @str: Pointer of value
  *
  * Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
 }
