4e5003b954981b8384b3e321db09911ff1de4310
Rwanbt <barat.erwan@gmail.com>
2026-04-08 15:55:15 +0200

    fix: proto const

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,37 +3,36 @@
 #include <unistd.h>
 
 /**
- * append_text_to_file - Appends text at the end of a file
- * @filename: The name of the file
- * @text_content: The text to append
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: The name of the file.
+ * @text_content: The NULL terminated string to add at the end of the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure.
  */
-int append_text_to_file(const char *filename, const char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, bytes_written;
+	int fd;
+	int len = 0;
+	ssize_t bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
-	}
+		while (text_content[len])
+			len++;
 
-	for (len = 0; text_content[len]; len++)
-		;
-
-	bytes_written = write(fd, text_content, len);
-	if (bytes_written == -1 || bytes_written != len)
-	{
-		close(fd);
-		return (-1);
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
