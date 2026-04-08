4ec624eacccc737c3442a0ab50d7051590d1d99b
Rwanbt <barat.erwan@gmail.com>
2026-04-08 16:55:51 +0200

    Fix: betty function length copy

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,19 @@
 #include <unistd.h>
 
 /**
+ * close_fd - Closes a file descriptor and exits on failure.
+ * @fd: The file descriptor to close.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @argc: Number of arguments.
  * @argv: Array of arguments.
@@ -21,52 +34,34 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* O_TRUNC vide le fichier s'il existe, 0664 définit les droits rw-rw-r-- */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
+		close_fd(fd_from);
 		exit(99);
 	}
-
 	while ((n_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		n_wrote = write(fd_to, buffer, n_read);
 		if (n_wrote == -1 || n_wrote != n_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
 			exit(99);
 		}
 	}
-
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	close_fd(fd_from);
+	close_fd(fd_to);
 	return (0);
 }
